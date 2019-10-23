46a11418c8fdaf243e64a7133533ff5b027d46e7
deezy13 <deezy13@gmail.com>
2019-10-23 03:37:32 +0000

    Automatically backed up by Learn

--- lib/meowing_cat.rb
+++ lib/meowing_cat.rb
@@ -1,19 +1,9 @@
-## code your solution here. 
-class Cat 
-  
-  attr_accessor = :name 
-  
-  def name=(name)
-     
-  end
-  
-  def name 
-    
-  end 
-  def meow
-    puts "meow!"
-  end 
-  
-end 
+class Cat
+  attr_reader :name
+  attr_accessor :mood
 
-new_instance = Cat.name
+  def initialize(name)
+    @name = name
+    @mood = "nervous"
+  end
+end
