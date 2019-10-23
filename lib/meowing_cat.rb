219ab4f953630ffbf1a85d8e36f800120b887ed1
deezy13 <deezy13@gmail.com>
2019-10-23 03:39:28 +0000

    Automatically backed up by Learn

--- lib/meowing_cat.rb
+++ lib/meowing_cat.rb
@@ -1,11 +1,14 @@
 class Cat
-  attr_reader :name
+ 
   attr_accessor :mood
 
   def initialize(name)
     @name = name
-    @mood = "nervous"
-  end
+      end
+      
+      def meow
+        puts "meow!"
+      end 
 end
 
 
